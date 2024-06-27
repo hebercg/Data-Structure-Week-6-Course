f4b27b530495cf34468b4c60374b8f8cf7f4e572
hebercg <116598621+hebercg@users.noreply.github.com>
2024-06-27 22:46:24 +0200

    testing code

--- src/main/java/DS23244Week6/RecursionTutorial.java
+++ src/main/java/DS23244Week6/RecursionTutorial.java
@@ -1,23 +1,22 @@
 package DS23244Week6;
 
 /**
- *
  * @author hebergoncalves
  */
 public class RecursionTutorial {
 
-        //given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
-    public  int countHi(String str){
+    //given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+    public int countHi(String str) {
         //base case
-        if (str.length() < 2 ){
+        if (str.length() < 2) {
             System.out.println("String too small to recurse on..");
             return 0;
         }
         //success - we found hi, but call recursively countHi
-        else if (str.substring(0, 2).equalsIgnoreCase("hi")){
+        else if (str.substring(0, 2).equalsIgnoreCase("hi")) {
             //did find hi, so we need to move down 2 positions in the string str
-                return 1 + countHi(str.substring(2));
-            }
+            return 1 + countHi(str.substring(2));
+        }
         //fail - no hi, but call recursively countHi
         else {
             //did not find hi, so only need to move down 1 position in teh string str
@@ -25,18 +24,16 @@ public class RecursionTutorial {
         }
     }//countHi
 
-    public  String removeX(String str){
+    public String removeX(String str) {
         str = str.toLowerCase();
         //base case
-        if (str.length() < 1){
+        if (str.length() < 1) {
             System.out.println("String too small to recurse on..");
             return str;
-        //found x, remove it and call removeX recursively
-        }
-        else if (str.charAt(0) == 'x') {
+            //found x, remove it and call removeX recursively
+        } else if (str.charAt(0) == 'x') {
             return removeX(str.substring(1));
-        }
-        else{
+        } else {
             //did not find x, but still need to call removeX recursively
             return str.charAt(0) + removeX(str.substring(1));
             //return str  + "end";
@@ -44,5 +41,17 @@ public class RecursionTutorial {
 
     }
 
+    public boolean containsValue(int[] arr, int index, int value) {
+        // base case: reached end of array
+        if (index >= arr.length) {
+            return false;
+        }
+        // check if current element is the value we're looking for
+       else if (arr[index] == value) {
+            return true;
+        }
+        // recursively check the next part of the array
+        return containsValue(arr, index + 1, value);
+    }
 }
 
--- src/main/java/DS23244Week6/RecursionTutorialApp.java
+++ src/main/java/DS23244Week6/RecursionTutorialApp.java
@@ -1,10 +1,9 @@
 package DS23244Week6;
 
 /**
- *
  * @author hebergoncalves
  */
-public class RecursionTutorialApp{
+public class RecursionTutorialApp {
     public static void main(String[] args) {
         RecursionTutorial myRT = new RecursionTutorial();
         System.out.println("#########testing countHi##########");
@@ -19,5 +18,12 @@ public class RecursionTutorialApp{
         System.out.println("removeX (ab): " + myRT.removeX("ab"));
         //test remove X
         System.out.println("removeX (xjxxxxtxx): " + myRT.removeX("xjxxxxtxx"));
+
+        //test array
+        System.out.println("#########testing containsValue##########");
+        int[] array1 = {2, 3, 6, 9, 21 };
+        int[] array2 = {10, 20, 30, 40, 50};
+        System.out.println("containsValue (array1, 3): " + myRT.containsValue(array1, 0, 3));
+        System.out.println("containsValue (array2, 25): " + myRT.containsValue(array2, 0, 25));
     }
 }
